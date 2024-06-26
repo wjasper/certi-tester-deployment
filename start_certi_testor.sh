3695e8be756931cdfd1e0bd6e2ec99498ca9e4aa
nuttysunday <shivamghodke10000@gmail.com>
2024-06-26 02:29:38 -0400

    shell script created for mac

--- start_certi_testor.sh
+++ start_certi_testor.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
 # Determine script directory
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="/Users/sunday/brainfuck/certi_testor_deployement"
+URL="http://127.0.0.1:7784"
 
-# Function to activate virtual environment
-activate_venv () {
-    if [ -f "$SCRIPT_DIR/certi_testor_deployement_env/bin/activate" ]; then
-        # Unix-like (Linux, macOS)
-        source "$SCRIPT_DIR/certi_testor_deployement_env/bin/activate"
-    elif [ -f "$SCRIPT_DIR/certi_testor_deployement_env/Scripts/activate" ]; then
-        # Windows
-        source "$SCRIPT_DIR\certi_testor_deployement_env\Scripts\activate"
-    else
-        echo "Virtual environment not found."
-        exit 1
-    fi
+# AppleScript command to open a new Terminal window, activate it, and execute the script
+CMD="
+tell application \"Terminal\"
+   do script \"cd $SCRIPT_DIR && source $SCRIPT_DIR/certi_testor_deployement_env/bin/activate && python3 app.py; exec bash\"
+   activate
+end tell
+"
+
+# Run the AppleScript command to execute the main script
+osascript -e "$CMD"
+
+# Delay in seconds before opening the URL
+DELAY=2
+
+# Function to open and refresh the browser
+refresh_browser() {
+    open "$URL"
+    sleep "$DELAY"
+    osascript -e '
+        tell application "Safari"
+            set URL of front document to "http://127.0.0.1:7784"
+        end tell
+    '
 }
 
-# Activate virtual environment
-activate_venv
-
-# Change to script directory
-cd "$SCRIPT_DIR"
-
-# Run your Flask application
-python3 app.py
+# Open and refresh the browser
+refresh_browser
