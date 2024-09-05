d714a520ed1284b0bf048b6cdb4bdf302af0d613
nuttysunday <shivamghodke10000@gmail.com>
2024-09-05 10:13:02 -0400

    fixed major bug, of inserting df into database, made it column based, instead of just placeholder, leading to mismatch depending on the sequence, added bound of 0-100 for penetration value

--- data_pipeline/file_parser.py
+++ data_pipeline/file_parser.py
@@ -48,41 +48,45 @@ def parse_penetration_data(file):
     return data
 
 
-def parse_load_data(file):
-    data = []
-    parsing_data = False
+def parse_load_data(data):
+    parsed_data = []
     
-    for line in file:
-        if not parsing_data and 'F' in line:
-            parsing_data = True
+    for entry in data:
+        parts = entry.strip().split()
+        flow_rate = resistance = photo_reading = penetration = mass_challenged_filter = 0
+        year = month = day = hour = minute = second = None
+        skip_entry = False 
         
-        if parsing_data and line.strip() and not line.startswith('#'):
-            parts = line.strip().split()
-            flow_rate = resistance = photo_reading = penetration = mass_challenged_filter = 0
-            for i in range(len(parts) - 1):
-                if parts[i] == 'F':
-                    flow_rate = float(parts[i + 1])
-                elif parts[i] == 'R':
-                    resistance = float(parts[i + 1])
-                elif parts[i] == 'U':
-                    photo_reading = float(parts[i + 1])
-                elif parts[i] == 'P':
-                    penetration = float(parts[i + 1])
-                elif parts[i] == 'M':
-                    mass_challenged_filter = float(parts[i + 1])
-                elif parts[i] == 'mv':
-                    month = int(parts[i+1])
-                    day = int(parts[i+2])
-                    year = 2000 + int(parts[i+3])
-                    hour = int(parts[i+4])
-                    minute = int(parts[i+5])
-                    second = int(parts[i+6])
-            
-            timestamp = pd.to_datetime(f"{year:04d}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d}")
-            
-            data.append([flow_rate, resistance, photo_reading, penetration, mass_challenged_filter, timestamp])
+        for i in range(len(parts) - 1):
+            if parts[i] == 'F':
+                flow_rate = float(parts[i + 1])
+            elif parts[i] == 'R':
+                resistance = float(parts[i + 1])
+            elif parts[i] == 'U':
+                photo_reading = float(parts[i + 1])
+            elif parts[i] == 'P':
+                penetration = float(parts[i + 1])
+                if penetration > 100 or penetration <0:
+                    skip_entry = True
+                    break
+            elif parts[i] == 'M':
+                mass_challenged_filter = float(parts[i + 1])
+            elif parts[i] == 'mv':
+                month = int(parts[i+1])
+                day = int(parts[i+2])
+                year = 2000 + int(parts[i+3])
+                hour = int(parts[i+4])
+                minute = int(parts[i+5])
+                second = int(parts[i+6])
+        
+        if skip_entry:
+            continue 
+        
+        timestamp = pd.to_datetime(f"{year:04d}-{month:02d}-{day:02d} {hour:02d}:{minute:02d}:{second:02d}")
+        
+        parsed_data.append([flow_rate, resistance, photo_reading, penetration, mass_challenged_filter, timestamp])
 
-    return data
+    return parsed_data
 
 
 def parse_g_data(file, weight_delta):
@@ -136,7 +140,6 @@ def process_penetration_data(data):
 def process_g_data(data):
     df = pd.DataFrame(data, columns=['Flow Rate (liter/min)', 'Resistance (mm of H2O)', 'Photometric Reading (mV)', 'Concentration', 'Minutes Passed', 'Delta weight', 'Timestamp'])
     df['Minutes Passed'] = df['Minutes Passed'].round(2)
-    df.drop(df.tail(1).index, inplace=True)
     return df
 
 def process_load_data(data):
