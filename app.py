a466425244bd06420540712db7f1be972088e54a
nuttysunday <shivamghodke10000@gmail.com>
2024-10-03 13:00:14 -0400

    successfully dockerized the infinite timer code, along with showing data in the frontend in real-time using socketIO

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from flask_socketio import SocketIO
 import database_management 
 from data_pipeline import data_processing, analyze_data, plot_graph, manage_test_records
 from certi_tester import certi_device
+import eventlet
 
 app = Flask(__name__, static_folder='dist/assets', template_folder='dist')
+socketio = SocketIO(app, cors_allowed_origins=["http://localhost:7784"])
 
 # Serve the build version of frontend code (REACT)
 @app.route('/')
@@ -87,17 +91,57 @@ def export_test_records():
     export_data = manage_test_records.export_test_records(selected_values)
     return jsonify(export_data)
 
-
-# WORKING ONLY WITH TIMER AS OF NOW
-@app.route('/api/certi-tester-device', methods=['POST'])
+@app.route('/api/start-certi-tester-connection', methods=['POST'])
 def certi_tester_device():
-    meta_data = request.get_json()
+    test_meta_data = request.get_json()
+    print(test_meta_data)
+    # this function creates buffer, and starts reading immediatly if timer is give
+    # always going to return the buffer, but only need to render it sometimes
     
-    buffer = certi_device.buffer_initialization(meta_data)
+    # could be with data without data
+    certi_tester_output = certi_device.connect_certi_tester(test_meta_data)
     
-    print("Buffer by the machine sent", buffer)
+    print("Buffer by the machine sent", certi_tester_output)
     
+    return jsonify(certi_tester_output)
+
+@socketio.on('connect')
+def handle_connect():
+    print("connect_certi_tester called", flush=True)
+    print('Client connected')
+    # Start a background task to monitor live data
+    socketio.start_background_task(monitor_live_data)
+
+def monitor_live_data():
+    last_data_length = 0  # Variable to track the last emitted data length
+
+    while True:
+        if certi_device.is_reading_active:
+            data = certi_device.get_live_data()
+
+            # Emit data only if the length has changed
+            if len(data) != last_data_length:  # Check if the current data length is different from the last emitted length
+                if data:  # Check if data is not empty
+                    print("Emitting live data:", data, flush=True)
+                    socketio.emit('live_data', data)  # Emit data immediately
+                else:
+                    print("No live data available", flush=True)
+
+                last_data_length = len(data)  # Update the last emitted data length
+
+        socketio.sleep(1)  # Check for new data every second
+
+@app.route('/api/start-timer', methods=['POST'])
+def start_timer_endpoint():
+    # pass the buffer, date_time to frontend ig
+    start_test_parameters = request.get_json()
+    print(start_test_parameters)
+    # Call the function to start the timer
+    print("start timer")
+    buffer = certi_device.start_reading(timer=None,buffer=start_test_parameters[0], date_time=start_test_parameters[1])  
+    print("Buffer by the machine sent if timer hit in the middle")
     return jsonify(buffer)
+
 
 @app.route('/api/end-timer', methods=['GET'])
 def stop_timer_endpoint():
@@ -105,6 +149,6 @@ def stop_timer_endpoint():
     print("Buffer by the machine sent if timer hit in the middle")
     return jsonify("buffer")
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=7784)
+    socketio.run(app, host='0.0.0.0', debug=True, port=7784)
+    #app.run(host='0.0.0.0', debug=True, port=7784)
--- certi_tester/certi_device.py
+++ certi_tester/certi_device.py
@@ -5,16 +5,24 @@ import sys
 import select
 import serial
 import threading
+import random
 
 # Global flag to end the timer
 stop_flag = threading.Event()
+live_data = []
+is_reading_active = False
 
 def start_reading(timer, buffer, date_time):
+    global live_data, is_reading_active 
     print("in the function start reading")
     print(buffer)
     
-    end_time = time.time() + int(timer)*60
-    
+    # If timer is provided, calculate the end_time; else, run infinitely
+    if timer:
+        end_time = time.time() + int(timer) * 60
+    else:
+        end_time = None  # No end time, run indefinitely
+
     try:
         serialPort = serial.Serial(
             port="/dev/ttyUSB0",
@@ -26,7 +34,9 @@ def start_reading(timer, buffer, date_time):
             rtscts=True,
             dsrdtr=True)
         
-        while time.time() < end_time:
+        is_reading_active = True
+        
+        while end_time is None or time.time() < end_time:
             # Wait until there is data waiting in the serial buffer
             if stop_flag.is_set():  # Check if stop flag is set
                 print("Timer stopped")
@@ -42,8 +52,10 @@ def start_reading(timer, buffer, date_time):
                     current_time = datetime.datetime.now()
                     date_string = current_time.strftime(" %m %d %Y %H %M %S")
                     buffer["Data"].append(certiString[:-2] + date_string)
+                    live_data.append(certiString[:-2] + date_string)
                 else:
                     buffer["Data"].append(certiString[:-2])
+                    live_data.append(certiString[:-2] + date_string)
             
             time.sleep(1)
         
@@ -52,14 +64,25 @@ def start_reading(timer, buffer, date_time):
     except Exception as e:
         print(f"Error occurred: {e}")
         print("Falling back to simulated data")
-        while time.time() < end_time:
+        
+        is_reading_active = True
+        
+        while end_time is None or time.time() < end_time:
             if stop_flag.is_set():  # Check if stop flag is set
                 print("Timer stopped")
                 break
             
             # Simulate reading data
-            print(time.time())
-            time.sleep(1)  # Sleep to prevent busy-waiting
+            print("time: ",  time.time())
+                    
+            delay = random.uniform(0, 10)
+            time.sleep(delay) 
+            
+            f_time = time.time() - 1
+            p_time = time.time() + 1
+            current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            formatted_string = f"F {f_time} R 100 P {p_time} {current_datetime}"
+            live_data.append(formatted_string)
         
         test_type = buffer['TestType']
         file_name = ""
@@ -70,14 +93,16 @@ def start_reading(timer, buffer, date_time):
             file_name = 'PenetrationTest.json'
         else:
             file_name = 'LoadingTest.json'
-        
+                         
+        # overwrite the data    
         with open(f'./fake_data_source_local/{file_name}', 'r') as file:
             data = json.load(file)    
-        
+            
         buffer['Data'] = data['Data']
-          
+            
         buffer["DataSourceStatus"] = "Using old data files. Also simulating time loop"            
-        
+    
+    is_reading_active = False    
 
     print("Timer completed")
     print(buffer)
@@ -85,15 +110,11 @@ def start_reading(timer, buffer, date_time):
     return buffer
 
 
-
-def buffer_initialization(meta_data):    
-    
-    global stop_flag
-
+def buffer_initialization(meta_data):
     date_time = False
     initial_weight = None
     final_weight = None
-
+    
     if meta_data['testType'] == "G":
         initial_weight = float(meta_data['initialWeight'])
     elif meta_data['testType'] == 'P':
@@ -104,17 +125,41 @@ def buffer_initialization(meta_data):
                 }
     
     print(buffer)
+      
+    return buffer, date_time
+
+def connect_certi_tester(meta_data): 
     
-    timer = meta_data['minutes']
-    
-    # Reset stop flag before starting the timer
+    global stop_flag
     stop_flag.clear()
     
-    update_buffer = start_reading(timer, buffer, date_time)
+    # before the buffer initialzation we clear the live data
+    global live_data
+    live_data = []
     
-    return update_buffer
+    # NO matter what, buffer needs to be initalized
+    # Even if starting for x minutes or infinite
+    
+    buffer, date_time = buffer_initialization(meta_data)
+   
+    # if minutes is given, then we immediatly start reading.    
+    if 'minutes' in meta_data:   
+        timer = meta_data['minutes']
+        print(timer)
+        # Reset stop flag before starting the timer
+        update_buffer = start_reading(timer, buffer, date_time)
+        return update_buffer
 
+    else:
+        print("timer")
+        return buffer, date_time
+    
 
 def end_reading():
     global stop_flag
     stop_flag.set() 
+    
+def get_live_data():
+    global live_data
+    #print("live_data:", live_data)
+    return live_data
